162705db32ee8ab8e4923256837346ff1b3ff021
FelixHommel <felixhommel04@gmail.com>
2026-03-22 16:19:36 +0100

    fix: typo

--- cmake/DefaultProjectSettings.cmake
+++ cmake/DefaultProjectSettings.cmake
@@ -1,5 +1,5 @@
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-    message(STATUS "Setting buidl type to 'Debug' because none other was specified.")
+    message(STATUS "Setting build type to 'Debug' because none other was specified.")
     set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
 
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release")
